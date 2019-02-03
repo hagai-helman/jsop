ebf3fda8f12da8d0f73bfeef74d2b9a85ead0874
Hagai Helman Tov <hagai.helman@gmail.com>
2019-02-03 15:56:41 +0200

    setup.py: add hard-coded version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='jsop',
-    version=__version__,
+    version='0.1',
     description='A dbm-based time-efficient persistence for large amount of JSON-style data',
     long_description="""This package can be used instead of JSON where the amount of data makes the I/O operartions too slow. Also, it is designed to enable easy migration of data in existing applications, with minimal code changes.
 """,
