b5f0f80e0a605e6a03fa042d26f65ad7f82d6a51
Hagai Helman Tov <hagai.helman@gmail.com>
2019-02-14 01:50:41 +0200

    Update version number to 0.7.0.

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='jsop',
-    version='0.6.0',
+    version='0.7.0',
     author='Hagai Helman Tov',
     author_email='hagai.helman@gmail.com',
-    description='A way to store large amounts of JSON-style data on disk, and to access it very quickly',
+    description='A way to store large amounts of JSON-style data on disk, and to access it quickly.',
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url='https://github.com/hagai-helman/jsop',
